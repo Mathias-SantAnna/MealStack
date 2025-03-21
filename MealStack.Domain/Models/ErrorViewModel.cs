01365de54004d6ed54c812b6bd358206098fc15f
Mathias Sant'Anna <dark_tricolor@hotmail.com>
2025-03-21 23:51:14 +0000

    added project solution and to git

---
+++ MealStack.Domain/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MealStack.Domain.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ MealStack.Infrastructure/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MealStack.Infrastructure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
