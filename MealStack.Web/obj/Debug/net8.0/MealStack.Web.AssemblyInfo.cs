bb8930a74d1c8a4a2850984b1b5d87ab07f58f8a
Mathias Sant'Anna <dark_tricolor@hotmail.com>
2025-04-15 15:55:24 +0100

    Add application structure diagrams

--- MealStack.Web/obj/Debug/net8.0/MealStack.Web.AssemblyInfo.cs
+++ MealStack.Web/obj/Debug/net8.0/MealStack.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MealStack.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a43ad2050ab714f3037834802a6f64ee86942f52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf339ed77668c17a0e630ab2e0eeb93b55b60b1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MealStack.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MealStack.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
